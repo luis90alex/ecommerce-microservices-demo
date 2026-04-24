bea9089cf6ca8d110d1735e743ce5b75d4a6caa4
luis90alex <luis90alex@gmail.com>
2026-04-24 18:59:05 +0200

    ÂÂÂAdded course microservices

---
+++ microservice-student/src/main/java/com/microservice/student/entities/Student.java
@@ -0,0 +1,58 @@
+package com.microservice.student.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(name = "last_name")
+    private String lastName;
+    private String email;
+    @Column(name = "course_id")
+    private Long courseId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Long courseId) {
+        this.courseId = courseId;
+    }
+}
